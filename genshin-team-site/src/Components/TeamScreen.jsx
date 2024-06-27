a349d37ac2afa91d3b60df4080663f3c8bb6cc3b
nathan-andrei <nathan_andrei_asnan@dlsu.edu.ph>
2024-06-27 18:45:45 +0800

    Imported context, displayed crude information of currently active team, and added delete button with onClick delete function

--- genshin-team-site/src/Components/TeamScreen.jsx
+++ genshin-team-site/src/Components/TeamScreen.jsx
@@ -1,11 +1,35 @@
-export default function TeamScreen(){
+import { useContext } from "react";
+import { UserContext } from "../App";
+
+export default function TeamScreen(props){
+	let team = props.currentTeam;
+	
+	function DeleteButton(){
+		return(
+			<button onClick={() => props.delete(team.id)}>
+				Delete Team
+			</button>
+		)
+	}
+	
+	if(Object.keys(team).length === 0){
+		return(
+			<div className="team-screen">
+				Select a team
+			</div>
+		)
+	}
 	return(
 		<div className="team-screen">
+			{/*
 			//Character deck
 				//Character
 					//Character portrait
 					//Notes
-				asd
+			*/}
+			{team.name}
+			{team.characters}
+			<DeleteButton />
 		</div>
 	)
 }
