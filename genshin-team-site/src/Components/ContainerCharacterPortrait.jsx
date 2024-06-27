6e9eca43bd07f7cd559c56dc88776dcbc61571a2
nathan-andrei <nathan_andrei_asnan@dlsu.edu.ph>
2024-06-27 18:30:08 +0800

    Cleaned up file

--- genshin-team-site/src/Components/ContainerCharacterPortrait.jsx
+++ genshin-team-site/src/Components/ContainerCharacterPortrait.jsx
@@ -1,10 +1,8 @@
 export default function ContainerCharacterPortrait({character}){
 	return(
 		<div className="container-character-portrait">
-			{/*img here*/}
 			<img src={"./Portraits/" + character + ".png"} />
-			<div className="container-character-bg">
-			</div>
+			<div className="container-character-bg"></div>
 		</div>
 	)
 }
