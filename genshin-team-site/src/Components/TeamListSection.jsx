5f6cdc1f8080e383976d949926c10b1066c30635
nathan-andrei <nathan_andrei_asnan@dlsu.edu.ph>
2024-06-27 18:40:52 +0800

    Imported context, and cleaned up the code

--- genshin-team-site/src/Components/TeamListSection.jsx
+++ genshin-team-site/src/Components/TeamListSection.jsx
@@ -1,38 +1,18 @@
 import {default as Container} from "./TeamContainer";
-import {CookiesProvider, useCookies} from 'react-cookie';
 import {default as NewTeamButton} from "./NewTeamButton";
+import { useState, useContext } from "react";
+import { UserContext } from "../App";
 
 export default function TeamListSection(){
-	const [cookies, setCookie] = useCookies(['user']);
+	const teams = useContext(UserContext).teams;
+	const addToList = useContext(UserContext).addToList;
 	
-	{/*
-	user enters site
-		no cookies
-			have to make cookies; made when adding team
-		yes cookies
-			read the cookies
-	
-	When CRUD team or character
-		update cookies
-	*/}
-	
-	function updateCookies(list){
-		setCookie('user', list, { path: '/'});
-		console.log(cookies)
-	}
-		
 	return(
-		<CookiesProvider>
-			<div className="team-list-section">
-			{/*
-				//map the the teams
-					//{teams.map}() => {return(teamcontainer)}
-					//Add team list button (add team box)
-			*/}
-				<Container name={"Team 1"} id={1} characters={["Hu Tao", "Yanfei", "Yelan", "Barbara"]}/>
-				<Container name={"Team 2"} id={2} characters={["Ganyu", "Hu Tao", "Yelan", "Barbara"]}/>
-				<NewTeamButton addFunction={updateCookies} />
-			</div>
-		</CookiesProvider>
+		<div className="team-list-section">
+			{teams.map((team) =>{
+				return <Container team={team} />
+			})}
+			<NewTeamButton addFunction={addToList} />
+		</div>
 	)
 }
