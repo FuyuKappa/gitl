0ed85de7066187555f298fff9e364e172f31aab6
FuyuKappa <fuyufuyukoppe@gmail.com>
2024-06-29 13:21:51 +0800

    Added the banner img, notes, and centered name; as well as notes array

--- genshin-team-site/src/Components/TeamScreenCharacterSection.jsx
+++ genshin-team-site/src/Components/TeamScreenCharacterSection.jsx
@@ -1,10 +1,21 @@
-export default function TeamScreenCharacterSection({character}){
+export default function TeamScreenCharacterSection({character, note}){
 	return(
 		<div className="team-character-section">
-			{character}
 				{/*Place character name*/}
 				{/*Place character portrait*/}
 				{/*Place character note*/}
+			<div className="section-character-name">
+				<center>
+					{character}
+				</center>
+			</div>
+			<div className="section-character-portrait">
+				<img className="banner" src={"./Banner/" + character + " Banner.png"}></img>
+			</div>
+			<div className="section-character-notes">
+				{note}
+				{/*Function to display "Character notes" if no notes are found*/}
+			</div>
 		</div>
 	)
 }
