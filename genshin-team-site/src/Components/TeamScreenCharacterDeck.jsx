d6bce08870747d8b741c742e973f651be667ac24
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-07-01 22:28:04 +0800

    Added key prop to the character sections

--- genshin-team-site/src/Components/TeamScreenCharacterDeck.jsx
+++ genshin-team-site/src/Components/TeamScreenCharacterDeck.jsx
@@ -1,12 +1,13 @@
 import {default as CharacterSection } from "./TeamScreenCharacterSection";
 export default function TeamScreenCharacterDeck({props}){
 	let {team, notes} = props;
-	console.log(props);
 	let characterList = [];	
+	
 	for(let i = 0; i < 4; i++){
 		let character = team[i] ? team[i] : "Blank";
 		let note = notes[i];
-		characterList.push(<CharacterSection character={character} note={note} />);
+		let key = crypto.randomUUID();
+		characterList.push(<CharacterSection character={character} note={note} key={key} />);
 	}
 	
 	return(
