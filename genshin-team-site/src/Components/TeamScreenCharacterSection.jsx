feddfd7d72432532f2fc20e994dc6d85624bd0a4
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-07-01 22:27:24 +0800

    Added gradients at top and bottom of character banner, styling for the notes section, and some styling regarding the height of the elements

--- genshin-team-site/src/Components/TeamScreenCharacterSection.jsx
+++ genshin-team-site/src/Components/TeamScreenCharacterSection.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 export default function TeamScreenCharacterSection({character, note}){
+	const [currentNote, setNote] = useState(note);
+	
+	function NotesForm(){
+		return;
+	}	
+	
 	return(
 		<div className="team-character-section">
 				{/*Place character name*/}
@@ -9,13 +17,24 @@ export default function TeamScreenCharacterSection({character, note}){
 					{character}
 				</center>
 			</div>
+			
 			<div className="section-character-portrait">
-				<img className="banner" src={"./Banner/" + character + ".png"}></img>
+				 <img className="banner" src={"./Banner/" + character + ".png"}></img>
 			</div>
-			<div className="section-character-notes">
-				{note}
-				{/*Function to display "Character notes" if no notes are found*/}
+			
+			<div className="bg-gradient-up"></div>
+			
+			<div className="notes-section">
+				<div className="character-notes-label">
+					Notes:
+				</div>
+				<div className="section-character-notes">
+					<textarea name="newNote" className="notes-form" value={currentNote} onChange={(e) => setNote(e.target.value)}>
+					</textarea>
+					{/*Function to display "Character notes" if no notes are found*/}
+				</div>
 			</div>
+			
 		</div>
 	)
 }
