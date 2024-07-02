5cbfadd89d3771957d2b73625b97e898d4b3fb44
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-07-02 16:03:14 +0800

    Wrapped the team section in a form, added a save button, and a save function for saving the notes

--- genshin-team-site/src/Components/TeamScreen.jsx
+++ genshin-team-site/src/Components/TeamScreen.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext} from "react";
 import { UserContext } from "../App";
 import '../Styles/stylesTeamScreen.css';
 import { default as CharacterDeck } from "./TeamScreenCharacterDeck";
@@ -6,12 +6,43 @@ import { default as TeamName } from "./TeamName";
 
 export default function TeamScreen(props){
 	let team = props.currentTeam;
+	const context = useContext(UserContext);
 	
 	function DeleteButton(){
 		return(
 			<button className="delete-button" onClick={() => props.delete(team.id)}>
 				Delete Team
 			</button>
+		)
+	}
+	
+	function saveTeam(e){
+		e.preventDefault();
+		let notes = [
+			e.target.note0.value.trim(),
+			e.target.note1.value.trim(),
+			e.target.note2.value.trim(),
+			e.target.note3.value.trim()
+		]
+		context.setTeams(currentTeams=>{
+			return currentTeams.map((currentTeam) =>{
+				if(currentTeam.id === team.id)
+					return {...team, notes: notes}
+				return currentTeam;
+			}) 
+		})
+		
+		//update the team screen
+		context.setTeam(() => {return {...team, notes: notes}});
+		
+		//update the active
+		context.toggleTeamActive(team.id);
+	}
+	
+	function SaveButton(){
+		return(
+			<input type="submit" className="Save-button" value="Save changes">
+			</input>
 		)
 	}
 	
@@ -24,15 +55,12 @@ export default function TeamScreen(props){
 	}
 	return(
 		<div className="team-screen">
-			{/*
-			//Character deck
-				//Character
-					//Character portrait
-					//Notes
-			*/}
-			<TeamName teamName = {team.name}/>
-			<CharacterDeck props={{team: team.characters, notes: team.notes}}/>
-			<DeleteButton />
+				<TeamName teamName = {team.name}/>
+			<form onSubmit={(e)=>saveTeam(e)}>
+				<CharacterDeck props={{team: team.characters, notes: team.notes}}/>
+				<SaveButton />
+				<DeleteButton />
+			</form>
 		</div>
 	)
 }
