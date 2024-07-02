88eb7716632c6b046733ab4abeb70623cee16f25
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-07-02 15:55:47 +0800

    Added index prop for the character section

--- genshin-team-site/src/Components/TeamScreenCharacterDeck.jsx
+++ genshin-team-site/src/Components/TeamScreenCharacterDeck.jsx
@@ -1,4 +1,5 @@
 import {default as CharacterSection } from "./TeamScreenCharacterSection";
+
 export default function TeamScreenCharacterDeck({props}){
 	let {team, notes} = props;
 	let characterList = [];	
@@ -6,8 +7,9 @@ export default function TeamScreenCharacterDeck({props}){
 	for(let i = 0; i < 4; i++){
 		let character = team[i] ? team[i] : "Blank";
 		let note = notes[i];
+		let index = i;
 		let key = crypto.randomUUID();
-		characterList.push(<CharacterSection character={character} note={note} key={key} />);
+		characterList.push(<CharacterSection character={character} note={note} key={key} index={i}/>);
 	}
 	
 	return(
