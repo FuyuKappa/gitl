cf584394b9a4e9936873966a473482c3a7cde146
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-07-04 01:50:25 +0800

    Added data file for the character data

---
+++ genshin-team-site/src/Data/GenshinCharacters.jsx
@@ -0,0 +1,245 @@
+export const GenshinCharacterData = [
+	{
+		"name": "Albedo", "element": "Geo", "rarity": "5"
+	},
+	{
+		"name": "Bennett", "element": "Pyro", "rarity": "4"
+	},
+	{
+		"name": "Ayato", "alt": "Kamisato Ayato", "element": "Hydro", "rarity": "5"
+	},
+	{
+		"name": "Childe", "alt": "Tartaglia", "element": "Hydro", "rarity": "5"
+	},
+	{
+		"name": "Al Haitham", "element": "Dendro", "rarity": "5"
+	},
+	{
+		"name": "Amber","element": "Pyro", "rarity": "4"
+	},
+	{
+		"name": "Arlecchino", "element": "Pyro", "rarity": "5"
+	},
+	{
+		"name": "Ayaka", "alt": "Kamisato Ayaka", "element": "Cryo", "rarity": "5"
+	},
+	{
+		"name": "Baizhu", "element": "Dendro", "rarity": "5"
+	},
+	{
+		"name": "Barbara", "element": "Hydro", "rarity": "4"
+	},
+	{
+		"name": "Beidou", "element": "Electro", "rarity": "4"
+	},
+	{
+		"name": "Candace", "element": "Hydro", "rarity": "4"
+	},
+	{
+		"name": "Charlotte", "element": "Cryo", "rarity": "4"
+	},
+	{
+		"name": "Chevreuse", "element": "Pyro", "rarity": "4"
+	},
+	{
+		"name": "Chiori", "element": "Geo", "rarity": "5"
+	},
+	{
+		"name": "Chongyun", "element": "Cryo", "rarity": "4"
+	},
+	{
+		"name": "Clorinde", "element": "Clorinde", "rarity": "5"
+	},
+	{
+		"name": "Collei", "element": "Dendro", "rarity": "4"
+	},
+	{
+		"name": "Cyno", "element": "Electro", "rarity": "5"
+	},
+	{
+		"name": "Dehya", "element": "Pyro", "rarity": "5"
+	},
+	{
+		"name": "Diluc", "element": "Pyro", "rarity": "5"
+	},
+	{
+		"name": "Diona", "element": "Cryo", "rarity": "4"
+	},
+	{
+		"name": "Dori", "element": "Electro", "rarity": "4"
+	},
+	{
+		"name": "Eula", "element": "Cryo", "rarity": "5"
+	},
+	{
+		"name": "Faruzan", "element": "Anemo", "rarity": "4"
+	},
+	{
+		"name": "Fischl", "element": "Electro", "rarity": "4"
+	},
+	{
+		"name": "Freminet", "element": "Cryo", "rarity": "4"
+	},
+	{
+		"name": "Furina", "element": "Hydro", "rarity": "5"
+	},
+	{
+		"name": "Gaming", "element": "Pyro", "rarity": "4"
+	},
+	{
+		"name": "Ganyu", "element": "Cryo", "rarity": "5"
+	},
+	{
+		"name": "Gorou", "element": "Geo", "rarity": "4"
+	},
+	{
+		"name": "Heizou", "element": "Anemo", "rarity": "4"
+	},
+	{
+		"name": "Hu Tao", "element": "Pyro", "rarity": "5"
+	},
+	{
+		"name": "Itto", "alt": "Arataki Itto", "element": "Geo", "rarity": "5"
+	},
+	{
+		"name": "Jean", "element": "Anemo", "rarity": "5"
+	},
+	{
+		"name": "Kaeya", "element": "Cryo", "rarity": "4"
+	},
+	{
+		"name": "Kaveh", "element": "Dendro", "rarity": "4"
+	},
+	{
+		"name": "Kazuha", "element": "Anemo", "rarity": "5"
+	},
+	{
+		"name": "Keqing", "element": "Electro", "rarity": "5"
+	},
+	{
+		"name": "Kirara", "element": "Dendro", "rarity": "4"
+	},
+	{
+		"name": "Klee", "element": "Pyro", "rarity": "5"
+	},
+	{
+		"name": "Kokomi", "element": "Hydro", "rarity": "5"
+	},
+	{
+		"name": "Kuki", "alt": "Shinobu Kuki", "element": "Electro", "rarity": "4"
+	},
+	{
+		"name": "Layla", "element": "Cryo", "rarity": "4"
+	},
+	{
+		"name": "Lisa", "element": "Electro", "rarity": "4"
+	},
+	{
+		"name": "Lynette", "element": "Anemo", "rarity": "4"
+	},
+	{
+		"name": "Lyney", "element": "Pyro", "rarity": "5"
+	},
+	{
+		"name": "Mika", "element": "Cryo", "rarity": "4"
+	},
+	{
+		"name": "Mona", "element": "Hydro", "rarity": "5"
+	},
+	{
+		"name": "Nahida", "element": "Dendro", "rarity": "5"
+	},
+	{
+		"name": "Navia", "element": "Geo", "rarity": "5"
+	},
+	{
+		"name": "Neuvillette", "element": "Hydro", "rarity": "5"
+	},
+	{
+		"name": "Nilou", "element": "Hydro", "rarity": "5"
+	},
+	{
+		"name": "Ningguang", "element": "Geo", "rarity": "4"
+	},
+	{
+		"name": "Noelle", "element": "Geo", "rarity": "4"
+	},
+	{
+		"name": "Raiden Shogun", "alt": "Ei", "element": "Electro", "rarity": "5"
+	},
+	{
+		"name": "Razor", "element": "Electro", "rarity": "4"
+	},
+	{
+		"name": "Rosaria", "element": "Cryo", "rarity": "4"
+	},
+	{
+		"name": "Sara", "element": "Electro", "rarity": "4"
+	},
+	{
+		"name": "Sayu", "element": "Anemo", "rarity": "4"
+	},
+	{
+		"name": "Sethos", "element": "Electro", "rarity": "4"
+	},
+	{
+		"name": "Shenhe", "element": "Cryo", "rarity": "5"
+	},
+	{
+		"name": "Sigewinne", "element": "Hydro", "rarity": "5"
+	},
+	{
+		"name": "Sucrose", "element": "Anemo", "rarity": "4"
+	},
+	{
+		"name": "Thoma", "element": "Pyro", "rarity": "4"
+	},
+	{
+		"name": "Tighnari", "element": "Dendro", "rarity": "5"
+	},
+	{
+		"name": "Venti", "element": "Anemo", "rarity": "5"
+	},
+	{
+		"name": "Wanderer", "element": "Anemo", "rarity": "5"
+	},
+	{
+		"name": "Wriothesley", "element": "Cryo", "rarity": "5"
+	},
+	{
+		"name": "Xiangling", "element": "Pyro", "rarity": "4"
+	},
+	{
+		"name": "Xianyun", "element": "Anemo", "rarity": "5"
+	},
+	{
+		"name": "Xiao", "element": "Anemo", "rarity": "5"
+	},
+	{
+		"name": "Xingqiu", "element": "Hydro", "rarity": "4"
+	},
+	{
+		"name": "Xinyan", "element": "Pyro", "rarity": "5"
+	},
+	{
+		"name": "Yae Miko", "element": "Electro", "rarity": "5"
+	},
+	{
+		"name": "Yanfei", "element": "Pyro", "rarity": "4"
+	},
+	{
+		"name": "Yaoyao", "element": "Dendro", "rarity": "4"
+	},
+	{
+		"name": "Yelan", "element": "Hydro", "rarity": "5"
+	},
+	{
+		"name": "Yoimiya", "element": "Pyro", "rarity": "5"
+	},
+	{
+		"name": "Yun Jin", "element": "Geo", "rarity": "4"
+	},
+	{
+		"name": "Zhongli", "element": "Geo", "rarity": "5"
+	}
+];
