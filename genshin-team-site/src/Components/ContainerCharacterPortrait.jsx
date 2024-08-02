e1c4d66e2d4c37830333f27a6149a968b3e38639
FuyuKappa <fuyufuyukoppe@gmail.com>
2024-08-02 19:22:11 +0800

    Removed onClick event for editing the team characters

--- genshin-team-site/src/Components/ContainerCharacterPortrait.jsx
+++ genshin-team-site/src/Components/ContainerCharacterPortrait.jsx
@@ -1,11 +1,6 @@
-import { useContext } from "react";
-import { UserContext } from "../App";
-
 export default function ContainerCharacterPortrait({character, id, position}){
-	const context = useContext(UserContext);
-	
 	return(
-		<div className="container-character-portrait" onClick={() => {context.openModal(character, position) }}>
+		<div className="container-character-portrait" >
 			<img src={"./SideIcon/" + character + ".png"} alt={character}/>
 			<div className="container-character-bg"></div>
 		</div>
