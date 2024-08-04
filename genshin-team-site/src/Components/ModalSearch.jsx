4bfe40e9c2f615272a248a29fa65faf113308a38
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-08-05 02:50:35 +0800

    Added function so that character names are resized and fade-outs for the character grid

--- genshin-team-site/src/Components/ModalSearch.jsx
+++ genshin-team-site/src/Components/ModalSearch.jsx
@@ -1,11 +1,45 @@
 import {GenshinCharacterData as data} from "../Data/GenshinCharacters"
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import { UserContext } from "../App";
 
 export default function CharacterSearch({CharacterPortrait, setPreviewCharacter}){
 	const context = useContext(UserContext);
 	const [searchQuery, setPreviewSearchQuery] = useState("");
+	const gridRef = useRef(null);
 	
+	//Special thanks to: Ricardo Goncalves
+	//https://github.com/ricardobrg/fitText/
+	function fitText(outputSelector){
+		// max font size in pixels
+		const maxFontSize = 30;
+		// get the DOM output element by its selector
+		let outputDiv = outputSelector;
+		// get element's width
+		let width = outputDiv.clientWidth;
+		// get content's width
+		let contentWidth = outputDiv.scrollWidth;
+		// get fontSize
+		let fontSize = parseInt(window.getComputedStyle(outputDiv, null).getPropertyValue('font-size'),10);
+		// if content's width is bigger then elements width - overflow
+		if (contentWidth > width){
+			fontSize = Math.ceil(fontSize * width/contentWidth,10);
+			fontSize =  fontSize > maxFontSize  ? fontSize = maxFontSize  : fontSize - 1;
+			outputDiv.style.fontSize = fontSize+'px';   
+		}else{
+			// content is smaller then width... let's resize in 1 px until it fits 
+			while (contentWidth === width && fontSize < maxFontSize){
+				fontSize = Math.ceil(fontSize) + 1;
+				fontSize = fontSize > maxFontSize  ? fontSize = maxFontSize  : fontSize;
+				outputDiv.style.fontSize = fontSize+'px';   
+				// update widths
+				width = outputDiv.clientWidth;
+				contentWidth = outputDiv.scrollWidth;
+				if (contentWidth > width){
+					outputDiv.style.fontSize = fontSize-1+'px'; 
+				}
+			}
+		}
+	}
 	function search(e){
 		setPreviewSearchQuery(() => {return e.target.value});
 	}
@@ -13,7 +47,16 @@ export default function CharacterSearch({CharacterPortrait, setPreviewCharacter}
 	useEffect(() =>{
 		setPreviewSearchQuery("");
 	}, [context.modalActive]);
-
+	
+	useEffect(() =>{
+		if(gridRef.current.children.length > 0){
+			for(let i = 0; i < gridRef.current.children.length; i++){
+				let section = gridRef.current.children[i];
+				fitText(section.querySelector(".portrait-name"));
+			}
+		}
+	}, [searchQuery]);
+	
 	
 	function populateWithData(){
 		return data.map((character) => {
@@ -38,14 +81,17 @@ export default function CharacterSearch({CharacterPortrait, setPreviewCharacter}
 	return(
 		<>
 			<div className="modal-search-container">
-				<form className="modal-search-form">
-					<input type="text" placeholder="Type character name to search" value={searchQuery} onChange={e => search(e)}/>
-				</form>
+				<input className="modal-search-form" type="text" placeholder="Type character name to search" value={searchQuery} onChange={e => search(e)}/>
 			</div>
 			
+			<div className="modal-box-character-grid-falloff top">
+			</div>
 			
-			<div className="modal-box-character-grid">
+			<div className="modal-box-character-grid" ref={gridRef}>
 				{populateWithData()}
+			</div>
+			
+			<div className="modal-box-character-grid-falloff bottom">
 			</div>
 		</>
 	);
