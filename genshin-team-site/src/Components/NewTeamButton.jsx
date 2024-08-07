663da4395dd2404d6fe42dc7fa1e7121512b7fa4
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-08-08 02:41:09 +0800

    Removed debug comment

--- genshin-team-site/src/Components/NewTeamButton.jsx
+++ genshin-team-site/src/Components/NewTeamButton.jsx
@@ -1,6 +1,4 @@
 export default function NewTeamButton({addFunction}){
-	{/*Have states, prop
-	for team name and characters*/}
 	return(
 		<div className="new-button team-container" onClick={addFunction}>
 			Add New Team
