7c8e58f5a89bcc689e896d3f424e00eaca9dc008
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-08-09 02:37:26 +0800

    Fixed bug where text field doesn't clear when changing to a team with blank text fields

--- genshin-team-site/src/Components/TeamDescription.jsx
+++ genshin-team-site/src/Components/TeamDescription.jsx
@@ -1,16 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from '../App';
 
 export default function TeamDescription({teamDescription}){
 	const [description, setDescription] = useState(teamDescription);
+	const contextTeam = useContext(UserContext).team;
 	
 	useEffect(() => {
 		setDescription(teamDescription);
-	}, [teamDescription]);
+	}, [teamDescription, contextTeam]);
 	
 	return(
 		<div className="description-section">
 			<label htmlFor="description">Team Description: </label>
-			<textarea className="description-text" name="description" id="description"value={description} onChange={(e) => setDescription(e.target.value)}>
+			<textarea className="description-text" name="description"
+			id="description"value={description} onChange={(e) => setDescription(e.target.value)}
+			placeholder="Write a short description of the team">
 			</textarea>
 		</div>
 	);
--- genshin-team-site/src/Components/TeamRotation.jsx
+++ genshin-team-site/src/Components/TeamRotation.jsx
@@ -1,17 +1,20 @@
-import { useState, useEffect } from "react";
-
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from '../App';
 
 export default function TeamRotation({teamRotation}){
 	const [rotation, setRotation] = useState(teamRotation);
+	const contextTeam = useContext(UserContext).team;
 	
 	useEffect(() => {
 		setRotation(teamRotation);
-	}, [teamRotation]);
+	}, [teamRotation, contextTeam]);
 	
 	return(
 		<div className="rotation-section">
 			<label htmlFor="rotation">Rotation: </label>
-			<textarea className="rotation-text" name="rotation" id="rotation" value={rotation} onChange={(e) => setRotation(e.target.value)}>
+			<textarea className="rotation-text" name="rotation"
+			id="rotation" value={rotation} onChange={(e) => setRotation(e.target.value)}
+			placeholder="Write in the team rotation (e.g. Yelan E Q -> Yanfei Q E C 3[N2C] -> etc.)">
 			</textarea>
 		</div>
 	);
