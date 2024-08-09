88805543770f7794a9189c3f977995b695f1ca10
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-08-09 17:08:13 +0800

    Preview character names now get adjusted to fit

--- genshin-team-site/src/Components/CharacterModal.jsx
+++ genshin-team-site/src/Components/CharacterModal.jsx
@@ -1,11 +1,54 @@
 import {GenshinCharacterData as data} from "../Data/GenshinCharacters"
-import { useContext, useState } from "react";
+import { useContext, useState, useRef, useEffect } from "react";
 import { UserContext } from "../App";
 import { default as CharacterSearch } from "./ModalSearch";
  
 export default function CharacterModal({character, position}){
 	const context = useContext(UserContext);
 	const [previewCharacter, setPreviewCharacter] = useState("Blank");
+	const previewRef = useRef(null);
+	
+	//Special thanks to: Ricardo Goncalves
+	//https://github.com/ricardobrg/fitText/
+	function fitText(outputSelector){
+		// max font size in pixels
+		const maxFontSize = 30;
+		// get the DOM output element by its selector
+		let outputDiv = outputSelector;
+		// get element's width
+		let width = outputDiv.clientWidth;
+		// get content's width
+		let contentWidth = outputDiv.scrollWidth;
+		// get fontSize
+		let fontSize = parseInt(window.getComputedStyle(outputDiv, null).getPropertyValue('font-size'),10);
+		// if content's width is bigger then elements width - overflow
+		if (contentWidth > width){
+			fontSize = Math.ceil(fontSize * width/contentWidth,10);
+			fontSize =  fontSize > maxFontSize  ? fontSize = maxFontSize  : fontSize - 1;
+			outputDiv.style.fontSize = fontSize+'px';   
+		}else{
+			// content is smaller then width... let's resize in 1 px until it fits 
+			while (contentWidth === width && fontSize < maxFontSize){
+				fontSize = Math.ceil(fontSize) + 1;
+				fontSize = fontSize > maxFontSize  ? fontSize = maxFontSize  : fontSize;
+				outputDiv.style.fontSize = fontSize+'px';   
+				// update widths
+				width = outputDiv.clientWidth;
+				contentWidth = outputDiv.scrollWidth;
+				if (contentWidth > width){
+					outputDiv.style.fontSize = fontSize-1+'px'; 
+				}
+			}
+		}
+	}
+	
+	useEffect(() => {
+		for(let i = 0; i < previewRef.current.children.length; i++){
+			let section = previewRef.current.children[i];
+			if(section.className === "character-select-icon character-select-preview")
+				fitText(section.querySelector(".portrait-name"));
+		}
+	}, [previewCharacter]);
 	
 	function editCharacter(newCharacter){
 		if(newCharacter !== "Blank"){
@@ -63,13 +106,13 @@ export default function CharacterModal({character, position}){
 						<line x1="5" y1="36" x2="36" y2 ="5" style={{stroke: "#FFF", strokeWidth: "5", strokeLinecap:"round" }}/>
 					</svg>
 				</div>
-				<div className="modal-box-preview">
+				<div className="modal-box-preview" ref={previewRef}>
 					<CharacterPortrait name={character} className="character-select-icon character-select-preview" />
 					->
 					<CharacterPortrait name={previewCharacter} className="character-select-icon character-select-preview" />
 				</div>
 				
-				<CharacterSearch CharacterPortrait={CharacterPortrait} setPreviewCharacter={setPreviewCharacter}/>
+				<CharacterSearch CharacterPortrait={CharacterPortrait} setPreviewCharacter={setPreviewCharacter} fitText={fitText}/>
 				
 				<div className="modal-box-button-container">
 					<button onClick={() => context.setModalActive(false)}>Cancel</button>
--- genshin-team-site/src/Components/ModalSearch.jsx
+++ genshin-team-site/src/Components/ModalSearch.jsx
@@ -2,44 +2,11 @@ import {GenshinCharacterData as data} from "../Data/GenshinCharacters"
 import { useContext, useState, useEffect, useRef } from "react";
 import { UserContext } from "../App";
 
-export default function CharacterSearch({CharacterPortrait, setPreviewCharacter}){
+export default function CharacterSearch({CharacterPortrait, setPreviewCharacter, fitText}){
 	const context = useContext(UserContext);
 	const [searchQuery, setPreviewSearchQuery] = useState("");
 	const gridRef = useRef(null);
 	
-	//Special thanks to: Ricardo Goncalves
-	//https://github.com/ricardobrg/fitText/
-	function fitText(outputSelector){
-		// max font size in pixels
-		const maxFontSize = 30;
-		// get the DOM output element by its selector
-		let outputDiv = outputSelector;
-		// get element's width
-		let width = outputDiv.clientWidth;
-		// get content's width
-		let contentWidth = outputDiv.scrollWidth;
-		// get fontSize
-		let fontSize = parseInt(window.getComputedStyle(outputDiv, null).getPropertyValue('font-size'),10);
-		// if content's width is bigger then elements width - overflow
-		if (contentWidth > width){
-			fontSize = Math.ceil(fontSize * width/contentWidth,10);
-			fontSize =  fontSize > maxFontSize  ? fontSize = maxFontSize  : fontSize - 1;
-			outputDiv.style.fontSize = fontSize+'px';   
-		}else{
-			// content is smaller then width... let's resize in 1 px until it fits 
-			while (contentWidth === width && fontSize < maxFontSize){
-				fontSize = Math.ceil(fontSize) + 1;
-				fontSize = fontSize > maxFontSize  ? fontSize = maxFontSize  : fontSize;
-				outputDiv.style.fontSize = fontSize+'px';   
-				// update widths
-				width = outputDiv.clientWidth;
-				contentWidth = outputDiv.scrollWidth;
-				if (contentWidth > width){
-					outputDiv.style.fontSize = fontSize-1+'px'; 
-				}
-			}
-		}
-	}
 	function search(e){
 		setPreviewSearchQuery(() => {return e.target.value});
 	}
@@ -82,7 +49,7 @@ export default function CharacterSearch({CharacterPortrait, setPreviewCharacter}
 	return(
 		<>
 			<div className="modal-search-container">
-				<input className="modal-search-form" type="text" placeholder="Type character name to search" value={searchQuery} onChange={e => search(e)}/>
+				<input className="modal-search-form" name="character-search-query" type="text" placeholder="Type character name to search" value={searchQuery} onChange={e => search(e)}/>
 			</div>
 			
 			<div className="modal-box-character-grid-falloff top">
