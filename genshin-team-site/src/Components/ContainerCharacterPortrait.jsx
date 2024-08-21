7d83f410964bea37c08e234a1ea72215ea110d44
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-08-21 23:01:12 +0800

    Added TeamList context for currSite and file path logic

--- genshin-team-site/src/Components/ContainerCharacterPortrait.jsx
+++ genshin-team-site/src/Components/ContainerCharacterPortrait.jsx
@@ -1,7 +1,10 @@
+import { useContext } from "react";
+import { ListContext } from "./TeamListTab";
+
 export default function ContainerCharacterPortrait({character, id, position}){
 	return(
 		<div className="container-character-portrait" >
-			<img src={"./SideIcon/" + character + ".png"} alt={character}/>
+			<img src={"./" + useContext(ListContext).currSite +"/SideIcon/" + character.name + ".png"} alt={character.name}/>
 			<div className="container-character-bg"></div>
 		</div>
 	)
