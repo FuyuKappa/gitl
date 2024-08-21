02bd41b96801877ef975019554c547c37a9b4d07
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-08-21 22:49:58 +0800

    Added data and assets for wuthering waves

---
+++ genshin-team-site/src/Data/WutheringWavesCharacters.jsx
@@ -0,0 +1,65 @@
+export const WuwaCharacterData = [
+	{
+		"name": "Aalto", "element": "Aero", "rarity": "4"
+	},
+	{
+		"name": "Baizhi", "element": "Glacio", "rarity": "4"
+	},
+	{
+		"name": "Calcharo", "element": "Electro", "rarity": "5"
+	},
+	{
+		"name": "Changli", "element": "Fusion", "rarity": "5"
+	},
+	{
+		"name": "Chixia", "element": "Fusion", "rarity": "4"
+	},
+	{
+		"name": "Danjin", "element": "Havoc", "rarity": "4"
+	},
+	{
+		"name": "Encore", "element": "Fusion", "rarity": "5"
+	},
+	{
+		"name": "Jianxin", "element": "Aero", "rarity": "5"
+	},
+	{
+		"name": "Jinhsi", "element": "Spectro", "rarity": "5"
+	},
+	{
+		"name": "Jiyan", "element": "Aero", "rarity": "5"
+	},
+	{
+		"name": "Lingyang", "element": "Glacio", "rarity": "5"
+	},
+	{
+		"name": "Mortefi", "element": "Fusion", "rarity": "5"
+	},
+	{
+		"name": "Rover", "element": "Havoc", "rarity": "5"
+	},
+	{
+		"name": "Rover", "element": "Spectro", "rarity": "5"
+	},
+	{
+		"name": "Sanhua", "element": "Glacio", "rarity": "4"
+	},
+	{
+		"name": "Taoqi", "element": "Havoc", "rarity": "4"
+	},
+	{
+		"name": "Verina", "element": "Spectro", "rarity": "5"
+	},
+	{
+		"name": "Yangyang", "element": "Aero", "rarity": "4"
+	},
+	{
+		"name": "Yinlin", "element": "Electro", "rarity": "5"
+	},
+	{
+		"name": "Yuanwu", "element": "Electro", "rarity": "4"
+	},
+	{
+		"name": "Zhezhi", "element": "Glacio", "rarity": "5"
+	}
+];
