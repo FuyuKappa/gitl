e544348e05c3223c3409f44f0b3215aa0d7699cb
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-08-22 02:01:18 +0800

    Added page for Zenless Zone Zero and respective data and assets

---
+++ genshin-team-site/src/Data/ZZZCharacters.jsx
@@ -0,0 +1,62 @@
+export const ZZZCharacterData = [
+	{
+		"name": "Anby", "alt": "Anby Demara", "element": "Electric", "type": "Stun", "rarity": "4"
+	},
+	{
+		"name": "Anton", "alt": "Anton Ivanov", "element": "Electric", "type": "Attack", "rarity": "4"
+	},
+	{
+		"name": "Ben", "alt": "Ben Bigger", "element": "Fire", "type": "Defence", "rarity": "4"
+	},
+	{
+		"name": "Billy", "alt": "Billy Kid", "element": "Physical", "type": "Attack", "rarity": "4"
+	},
+	{
+		"name": "Corin", "alt": "Corin Wickes", "element": "Physical", "type": "Attack", "rarity": "4"
+	},
+	{
+		"name": "Ellen", "alt": "Ellen Joe", "element": "Ice", "type": "Attack", "rarity": "5"
+	},
+	{
+		"name": "Grace", "alt": "Grace Howard", "element": "Electric", "type": "Anomaly", "rarity": "5"
+	},
+	{
+		"name": "Jane Doe", "element": "Physical", "type": "Anomaly", "rarity": "5"
+	},
+	{
+		"name": "Koleda", "alt": "Koleda Belobog", "element": "Fire", "type": "Stun", "rarity": "5"
+	},
+	{
+		"name": "Lucy", "alt": "Luciana de Montefio", "element": "Fire", "type": "Support", "rarity": "4"
+	},
+	{
+		"name": "Nekomata", "alt": "Nekomiya Mana", "element": "Physical", "type": "Attack", "rarity": "5"
+	},
+	{
+		"name": "Nicole", "alt": "Nicole Demara", "element": "Ether", "type": "Support", "rarity": "4"
+	},
+	{
+		"name": "Piper", "alt": "Piper Wheel", "element": "Physical", "type": "Anomaly", "rarity": "4"
+	},
+	{
+		"name": "Qingyi", "element": "Electric", "type": "Stun", "rarity": "5"
+	},
+	{
+		"name": "Rina", "alt": "Alexandrina Sebastiane", "element": "Electric", "type": "Support", "rarity": "5"
+	},
+	{
+		"name": "Seth", "alt": "Seth Lowell", "element": "Electric", "type": "Defence", "rarity": "4"
+	},
+	{
+		"name": "Soldier 11", "element": "Fire", "type": "Attack", "rarity": "5"
+	},
+	{
+		"name": "Soukaku", "element": "Ice", "type": "Support", "rarity": "4"
+	},
+	{
+		"name": "Lycaon", "alt": "Von Lycaon", "element": "Ice", "type": "Stun", "rarity": "5"
+	},
+	{
+		"name": "Zhu Yuan", "element": "Ether", "type": "Attack", "rarity": "5"
+	},
+];
---
+++ genshin-team-site/src/Pages/ZZZPage.jsx
@@ -0,0 +1,50 @@
+import TeamListTab from "../Components/TeamListTab";
+import TeamScreen from "../Components/TeamScreen";
+import CharacterModal from "../Components/CharacterModal";
+import Header from "../Components/SiteHeader";
+import { SiteContext } from "../App"
+
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+import { useContext, useEffect } from "react";
+import { useMediaQuery } from 'react-responsive';
+import {ZZZCharacterData as data} from "../Data/ZZZCharacters"
+
+export default function ZZZPage({currSite, setFromLocal}){
+	const siteContext = useContext(SiteContext);
+	let teams = siteContext.teams;
+	let setTeams = siteContext.setTeams;
+	const teamSize = 4;
+	
+	useEffect(() =>{
+		setFromLocal();
+	},[setFromLocal]);
+	
+	const isSmallerThan_1419 = useMediaQuery({ maxWidth: 1419 });
+	function toggleTeamList(){
+		if((isSmallerThan_1419 && siteContext.showTeams) || !isSmallerThan_1419) //render if showTeams is true and smaller than 1419. Render if bigger than 1419
+			return <TeamListTab teams={teams} setTeams={setTeams} teamSize={teamSize} currSite={currSite}/>;
+		else
+			return <></>;
+	}
+	
+	return(
+		<HelmetProvider>
+			<div className="body-wrapper">
+				<Helmet>
+					<link rel="stylesheet" href="../Styles/stylesIndex.css" />
+				</Helmet>
+				
+				<Header currentSite={currSite} />
+				<div className="content-wrapper">
+					{toggleTeamList()}
+					<TeamScreen teamSize={teamSize} currSite={currSite}/>
+					{ siteContext.modalActive ? 
+						(<CharacterModal character={siteContext.currentEditingCharacter} position={siteContext.currentEditingPosition} data={data} currSite={currSite}/>) :
+						(<></>)	
+					}
+				</div>
+			</div>
+		</HelmetProvider>
+	);
+}
