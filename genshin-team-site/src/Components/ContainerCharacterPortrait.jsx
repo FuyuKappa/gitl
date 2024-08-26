6e43d6ff9de1240cc54a6e2cb8e501b8c1446ae9
FuyuKappa <fuyufuyukoppe@gmail.com>
2024-08-27 01:47:40 +0800

    Added custom logic for ZZZ styling

--- genshin-team-site/src/Components/ContainerCharacterPortrait.jsx
+++ genshin-team-site/src/Components/ContainerCharacterPortrait.jsx
@@ -2,9 +2,20 @@ import { useContext } from "react";
 import { ListContext } from "./TeamListTab";
 
 export default function ContainerCharacterPortrait({character, id, position}){
+	let currSite = useContext(ListContext).currSite;
+	let imgPath = "./" + currSite +"/SideIcon/" + character.name + ".png";
+	if (currSite === "Zenless Zone Zero"){
+		imgPath = "url('" + imgPath + "')";
+		return(
+			<div className="container-character-portrait">
+				<div className="zzz-portrait" style={{"backgroundImage":imgPath}}>
+				</div>
+			</div>
+		)
+	}
 	return(
 		<div className="container-character-portrait" >
-			<img src={"./" + useContext(ListContext).currSite +"/SideIcon/" + character.name + ".png"} alt={character.name}/>
+			<img src={"./" + currSite +"/SideIcon/" + character.name + ".png"} alt={character.name}/>
 			<div className="container-character-bg"></div>
 		</div>
 	)
