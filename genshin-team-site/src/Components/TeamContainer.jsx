04906cb5c55c4ac9ec1dc2f7c82b00670c148182
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-08-29 22:51:37 +0800

    Added resonance deck

--- genshin-team-site/src/Components/TeamContainer.jsx
+++ genshin-team-site/src/Components/TeamContainer.jsx
@@ -10,18 +10,60 @@ export default function TeamContainer({team}){
 	const toggleTeamActive = useContext(SiteContext).toggleTeamActive;
 	const saveTeam = useContext(SiteContext).saveTeam;
 	const className = "team-container team-container-" + active;
-	const teamSize = useContext(ListContext).teamSize
+	const teamSize = useContext(ListContext).teamSize;
+	const currSite = useContext(ListContext).currSite;
 	
 	let characterList = [];	
 	for(let i = 0; i < teamSize; i++){
-		//console.log(characters);
 		let character = characters[i] ? characters[i] : {name: "AddCharacter"};
 		characterList.push(<CharacterPortrait  character={character} key={crypto.randomUUID()} id={id} position={i}/>)
 	}
 	
+	function ResonanceDeck(){
+		let elementList = [];
+		characters.forEach(character => {elementList.push(character === null ? "Null" : character.element)});
+		
+		let uniqueElements = {};
+		for(let i = 0; i < elementList.length; i++){
+			let currentElement = elementList[i];
+			
+			if(Object.keys(uniqueElements).indexOf(currentElement) === -1 && currentElement !== "Null"){
+				uniqueElements[currentElement] = 1;
+				for(let k = i + 1; k < elementList.length; k++){
+					if(currentElement === elementList[k])
+						uniqueElements[currentElement] = uniqueElements[currentElement] + 1;
+				}
+			}			
+		}
+		
+		let resonances = [];
+		let keys = Object.keys(uniqueElements);
+		if(keys.length === 4) resonances = elementList;
+		else if(keys.length < 4){
+			for(let i = 0; i < keys.length; i++){
+				if(uniqueElements[keys[i]] > 1){
+					resonances.push(keys[i]);
+					resonances.push(keys[i]);
+				}
+			}
+		}
+		let deck = [];
+		for(let i = 0; i < resonances.length; i++){
+			deck.push(<img className="deck-element" src={"./"+ currSite + "/Element/" + resonances[i] + ".png"} key={crypto.randomUUID()} alt={resonances[i]} />);
+		}
+		return(
+			<div className="resonance-deck">
+			{deck}
+			</div>
+		)
+	}
+	
 	return(
 		<div className={className} onClick={() => {saveTeam({switchActive: true, notifyUser: false, teamSize: teamSize}); previewTeam(team); toggleTeamActive(id);}}>
-			<Name teamName={name} id={id} selected={active}/>
+			<div className="team-container-top">
+				<Name teamName={name} id={id} selected={active}/>
+				{currSite === "Genshin Impact" ? <ResonanceDeck /> : <></>}
+			</div>
 			<hr style={{margin: '4px 8px'}} />
 				{/*Resnance deck (component = resonance deck)
 				//resonance icons
