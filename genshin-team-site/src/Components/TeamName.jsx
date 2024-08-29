5a634ac7a76134d8af1d78a993de211564feb8d2
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-08-29 22:52:31 +0800

    Added code to change class name of edit button so that it can be coloured accordingly

--- genshin-team-site/src/Components/TeamName.jsx
+++ genshin-team-site/src/Components/TeamName.jsx
@@ -6,17 +6,21 @@ export default function TeamName(props){
 	let containerID = props.id;
 	
 	let editFill;
+	let pencilClassName = "edit-btn";
 	if(props.selected === false) editFill = "white";
-	else editFill = "black";
+	else{ editFill = "black"; pencilClassName += " edit-btn-active";}
+	
 
 	const [editing, setEditing] = useState(false);
 	const [newName, setNewName] = useState("");
 	
 	const context = useContext(SiteContext);
 	const teamID = context.currentTeamID;
-	//const toggleTeamActive = context.toggleTeamActive;
-	//const previewTeam = context.previewTeam;
 	
+	function changeValue(e){
+		if(e.target.value.length < 50)
+			setNewName(e.target.value);
+	}
 	function toggleEdit(e){
 		e.stopPropagation();
 		setEditing(() => {return true});
@@ -29,7 +33,6 @@ export default function TeamName(props){
 	}
 	
 	function confirmEdit(id, newName, e){
-		console.log(newName);
 		e.stopPropagation();
 		let diffFlag = false;
 		if(!id && id !== containerID){
@@ -63,7 +66,7 @@ export default function TeamName(props){
 		return(
 			<div className="team-name">
 				<form onSubmit={(e) => confirmEdit(teamID, newName, e)} className="name-edit-form">
-					<input type="text" value={newName} onChange={e => setNewName(e.target.value)} onClick = {event => event.stopPropagation()}>
+					<input type="text" value={newName} onChange={e => changeValue(e)} onClick = {event => event.stopPropagation()}>
 					</input>
 					<div className="edit-btn check" onClick={(e) => confirmEdit(teamID, newName, e)}>
 						<svg width="16" height="16" className="check-mark" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
@@ -85,7 +88,7 @@ export default function TeamName(props){
 	return(
 		<div className="team-name">
 			{teamName}
-			<div className="edit-btn" onClick={toggleEdit}>
+			<div className={pencilClassName} onClick={toggleEdit}>
 				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill={editFill} className="bi bi-pencil" viewBox="0 0 16 16">
 				  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
 				</svg>
